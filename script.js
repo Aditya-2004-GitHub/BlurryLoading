844aa75073dd3891ed80d5e6a9a2e59760b7615c
Aditya-2004-GitHub <22f3001516@ds.study.iitm.ac.in>
2025-05-08 18:00:04 +0530

    Blurry Loading

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const loader = document.querySelector(".loader")
 const bg = document.querySelector(".bg")
 
-let load = 0
 
+let load = 0
 let int = setInterval(blurring, 30)
+
 
 function blurring() {
     load++
@@ -14,10 +15,11 @@ function blurring() {
 
     loader.innerText = `${load}%`
     loader.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)` //edit in css part of bg>>filter (property)
 
 }
 
+// Calculation part
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
